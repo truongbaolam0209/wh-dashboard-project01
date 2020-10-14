87d07dd198ae4476a702ba741bac1d3e2f98127a
Truong Bao Lam <truongbaolam0209@gmail.com>
2020-10-14 22:53:54 +0700

    Updates

---
+++ precache-manifest.02699514bee2a59c3db51528c95d3fe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490f1648ffe9ad8106465aff602e05b1",
+    "url": "/wh-dashboard-project01/index.html"
+  },
+  {
+    "revision": "6ba6c7bdd2aeefa83243",
+    "url": "/wh-dashboard-project01/static/css/2.df04db45.chunk.css"
+  },
+  {
+    "revision": "32da5a651fc2561efd0d",
+    "url": "/wh-dashboard-project01/static/css/main.ff914c26.chunk.css"
+  },
+  {
+    "revision": "6ba6c7bdd2aeefa83243",
+    "url": "/wh-dashboard-project01/static/js/2.d64a5929.chunk.js"
+  },
+  {
+    "revision": "4deb098d81cbe6e42b236cdb069f28e9",
+    "url": "/wh-dashboard-project01/static/js/2.d64a5929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32da5a651fc2561efd0d",
+    "url": "/wh-dashboard-project01/static/js/main.d409a936.chunk.js"
+  },
+  {
+    "revision": "baa44475c5f05fadaa67",
+    "url": "/wh-dashboard-project01/static/js/runtime-main.4f367d42.js"
+  }
+]);
